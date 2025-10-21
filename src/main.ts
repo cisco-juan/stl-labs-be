cde9fcebbc7b9a3439c211d1e7e52a25b7d82243
cisco <franciscornaut@gmail.com>
2025-10-21 16:33:40 -0400

    init

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('STL Lab API')
+    .setDescription('STL Lab API description')
+    .setVersion('1.0')
+    .addTag('STL Lab')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
